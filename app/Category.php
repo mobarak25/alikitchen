db5e2fd56db9e33b74f0258de107d7aed7ac4127
mobarak25 <mobarakali62@gmail.com>
2018-12-13 00:34:29 +0600

    relation

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
-    //
+class Category extends Model{
+
+	public function Items(){
+		return $this->hasMany('App\Item');
+		
+	}
+   
 }
